5b08be031bdd5f6dd4921236738627451331c358
Don Gumayagay <dngumayagay@gmail.com>
2022-10-30 20:39:13 +0800

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<main class="flex bg-pink-300 h-full ">
+	<section class="">
+		<h1>jempower's credit corp</h1>
+		<button>apply now</button>
+	</section>
+	<section class=" bg-yellow-300 ml-auto w-full max-w-xs flex flex-col">
+		<h1>choose des</h1>
+		<a class="bg-green-500 text-white text-center capitalize font-semibold" href="/client/login"
+			>client</a
+		>
+		<a class="bg-red-500 text-white text-center capitalize font-semibold" href="/admin/login"
+			>admin</a
+		>
+	</section>
+</main>
---
+++ src/routes/client/+page.svelte
@@ -0,0 +1,18 @@
+<div class=" bg-red-500  sm:bg-green-300 md:bg-blue-400">client dashboard</div>
+
+<section class="flex flex-col sm:flex-row">
+	<button> test </button>
+	<button> test2 </button>
+	<button> test2 </button>
+	<button> test2 </button>
+	<button> test2 </button>
+	<button> test2 </button>
+	<button> test2 </button>
+	<button> test2 </button>
+	<button> test2 </button>
+	<button> test2 </button>
+	<button> test2 </button>
+	<button> test2 </button>
+	<button> test2 </button>
+	<button> test2 </button>
+</section>
