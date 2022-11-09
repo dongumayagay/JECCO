73a726ff4d30fd753ee1d3c38bd7bd37f774685d
Francis Sanchez <sanchez.francispup@gmail.com>
2022-11-09 18:15:51 +0800

    SidebarNavigation in client side added

--- src/routes/client/+page.svelte
+++ src/routes/client/+page.svelte
@@ -1,18 +1,39 @@
-<div class=" bg-red-500  sm:bg-green-300 md:bg-blue-400">client dashboard</div>
+<!-- <script>
+	import Navbar from './Navbar.svelte'
+	import Sidebar from './Sidebar.svelte'
 
-<section class="flex flex-col sm:flex-row">
-	<button> test </button>
-	<button> test2 </button>
-	<button> test2 </button>
-	<button> test2 </button>
-	<button> test2 </button>
-	<button> test2 </button>
-	<button> test2 </button>
-	<button> test2 </button>
-	<button> test2 </button>
-	<button> test2 </button>
-	<button> test2 </button>
-	<button> test2 </button>
-	<button> test2 </button>
-	<button> test2 </button>
-</section>
+	let open = false
+</script>
+
+<Sidebar bind:open/>
+<Navbar bind:sidebar={open}/>
+
+<svelte:head>
+	<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"/>
+</svelte:head> -->
+<script>
+	import Sidebar from './Sidebar.svelte'
+
+	let open = false
+
+	
+
+</script>
+
+	<nav>
+	 <button on:click={()=>{open=true}}> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+		<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+	  </svg>
+	   </button>
+	</nav>
+
+<main>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis reprehenderit maiores molestias asperiores dolores debitis velit provident natus hic, voluptas sit odit fuga nostrum harum sapiente. A, mollitia vero. Maiores.
+
+
+
+</main>
+
+{#if open}
+	<Sidebar bind:open/>
+{/if}
---
+++ src/routes/client/Sidebar.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let open
+    
+    const closesidebar=()=>{open=false}
+</script>
+
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<main class=" fixed inset-0 bg-black/50 " on:click|self={closesidebar}>
+    <nav class=" flex flex-col bg-white w-full max-w-xs h-full p-4 ">
+        <button class=" ml-auto " on:click={closesidebar}> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+          
+
+        </button>
+        <a href="#payment">Payment</a>
+        <a href="#duedates">Duedates</a>
+
+    </nav>
+
+
+
+
+</main>
