f5810a571c1b54b8e3b8c409f9c4ca8886929da0
Francis Sanchez <sanchez.francispup@gmail.com>
2022-11-15 11:44:00 +0800

    backup file palang to

--- src/routes/admin/login/+page.svelte
+++ src/routes/admin/login/+page.svelte
@@ -1,28 +1,29 @@
 <script>
 	import { goto } from '$app/navigation';
-	let ausername = '';
-	let apassword = '';
+	let username = '';
+	let password = '';
 
 	async function login() {
-		console.log(ausername, apassword);
+		console.log(username, password);
 		await goto('/admin');
 	}
 </script>
 
-<main class="h-full grid place-items-center">
-	<form
-		class=" bg-red-500 w-full max-w-sm flex flex-col p-4 gap-2"
-		on:submit|preventDefault={login}
-	>
+<main class=" bg-gray-200 h-full flex">
+
+	<div class=" flex-1 grid place-items-center ">
+		<img class=" w-lg " src="/Admin.svg" alt="">
+	</div>
+
+<div class="flex flex-1  ">
+	<div class=" flex-1 grid place-items-center p-10">
+		<form class=" bg-white rounded-lg shadow-lg w-96 flex flex-col p-4 gap-4" on:submit|preventDefault={login}>
 		<h1 class="text-center font-bold">Admin Login</h1>
-		<input class=" rounded-lg" type="text" placeholder="username" bind:value={ausername} required />
-		<input
-			class="rounded-lg"
-			type="password"
-			placeholder="Password"
-			bind:value={apassword}
-			required
-		/>
-		<button class="bg-blue-500 py-2 rounded-lg text-white" type="submit">Login</button>
-	</form>
+		<input class=" rounded-lg" type="text" placeholder="username" bind:value={username} required />
+		<input class=" rounded-lg" type="password" placeholder="Password" bind:value={password} required /> 
+		<button class="bg-blue-500 py-2 mt-4 rounded-lg text-white transition duration-200 ease-in-out hover:bg-blue-900" type="submit">Login</button>
+		</form>
+	</div>
+</div>
+
 </main>
