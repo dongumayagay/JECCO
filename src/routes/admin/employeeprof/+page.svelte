95f202bbe675b3b26f99b479483ed734b774496b
Francis Sanchez <sanchez.francispup@gmail.com>
2022-12-05 02:41:42 +0800

    Client profile added and adjustments in tables

--- src/routes/admin/employeeprof/+page.svelte
+++ src/routes/admin/employeeprof/+page.svelte
@@ -13,15 +13,15 @@
 
 <section class="flex flex-col w-full h-screen p-6 ml-8 overflow-y-auto bg-gray-200 ">
 
-    <div class="flex items-center pl-4 shadow-md sm:rounded-lg h-10 bg-white gap-16">
+    <div class="flex items-center justify-end p-4 shadow-md sm:rounded-lg h-10 bg-white gap-4">
         
-        <button class=" btn btn-ghost btn-xs sm:btn-2xs md:btn-xs lg:btn-sm hover:bg-gray-300">
+        <button class=" btn btn-ghost bg-gray-200 btn-xs sm:btn-2xs md:btn-xs lg:btn-sm hover:bg-green-300">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" class="w-5 h-5">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>
         </button>
         
 
-        <button class=" btn btn-ghost btn-xs sm:btn-2xs md:btn-xs lg:btn-sm hover:bg-gray-300">
+        <button class=" btn btn-ghost bg-gray-200 btn-xs sm:btn-2xs md:btn-xs lg:btn-sm hover:bg-red-300">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
               </svg>
@@ -39,25 +39,25 @@
           
         </div>
     </div>
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+    <table class=" table w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th scope="col" class="p-4">
+                <th scope="col" class="">
                     <!-- <div class="flex items-center">
                         <input id="checkbox-all-search" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                         <label for="checkbox-all-search" class="sr-only">checkbox</label>
                     </div> -->
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" class="px-6">
                     Name
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" class="px-6">
                     Position
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" class="px-6">
                     Password
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" class="px-6">
                     
                 </th>
             </tr>
@@ -67,7 +67,6 @@
                 <td class="p-4 w-4">
                     <div class="flex items-center">
                         <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                        <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
                     </div>
                 </td>
                 <th scope="row" class="flex items-center py-4 px-6 text-gray-900 whitespace-nowrap dark:text-white">
@@ -77,15 +76,15 @@
                         <div class="font-normal text-gray-500">neil.sims@jecco.com</div>
                     </div>  
                 </th>
-                <td class="py-4 px-6">
+                <td class="px-6">
                     Admin staff
                 </td>
-                <td class="py-4 px-6">
+                <td class="px-6">
                     <div class="flex items-center">
                         <p class="" >password123</p>
                     </div>
                 </td>
-                <td class="py-4 px-6">
+                <td class="px-6">
 
                 <!-- Modal toggle -->
                 <label for="my-modal" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</label>
@@ -107,15 +106,15 @@
                         <div class="font-normal text-gray-500">bonnie@jecco.com</div>
                     </div>
                 </th>
-                <td class="py-4 px-6">
+                <td class="px-6">
                     Collector
                 </td>
-                <td class="py-4 px-6">
+                <td class="px-6">
                     <div class="flex items-center">
                         <p class="" >password123</p>
                     </div>
                 </td>
-                <td class="py-4 px-6">
+                <td class="px-6">
                     <!-- Modal toggle -->
                     <label for="my-modal" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</label>
                 </td>
@@ -134,7 +133,7 @@
                         <div class="font-normal text-gray-500">jese@jecco.com</div>
                     </div>
                 </th>
-                <td class="py-4 px-6">
+                <td class="px-6">
                     Collector
                 </td>
                 <td class="py-4 px-6">
@@ -142,7 +141,7 @@
                         <p class="" >password123</p>
                     </div>
                 </td>
-                <td class="py-4 px-6">
+                <td class="px-6">
                     <!-- Modal toggle -->
                     <label for="my-modal" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</label>
                 </td>
@@ -161,10 +160,10 @@
                         <div class="font-normal text-gray-500">thomes@jecco.com</div>
                     </div>
                 </th>
-                <td class="py-4 px-6">
+                <td class="px-6">
                     Admin staff
                 </td>
-                <td class="py-4 px-6">
+                <td class="px-6">
                     <div class="flex items-center">
                         <p class="" >password123</p>
                     </div>
@@ -188,15 +187,15 @@
                         <div class="font-normal text-gray-500">leslie@jecco.com</div>
                     </div>
                 </th>
-                <td class="py-4 px-6">
+                <td class="px-6">
                     Staff
                 </td>
-                <td class="py-4 px-6">
+                <td class="px-6">
                     <div class="flex items-center">
                         <p class="" >password123</p>
                     </div>
                 </td>
-                <td class="py-4 px-6">
+                <td class="px-6">
                     <!-- Modal toggle -->
                     <label for="my-modal" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</label>
                 </td>
--- src/routes/admin/inquiries/+page.svelte
+++ src/routes/admin/inquiries/+page.svelte
@@ -2,60 +2,59 @@
 	import SideNavAdmin from '../../../lib/components/SideNavAdmin.svelte';
 </script>
 
-<main class=" flex h-full w-full">
+<main class=" flex ">
 
-	<section class="flex flex-col w-64 h-full px-4 py-8 bg-green-700">
-		<SideNavAdmin/>
-	</section>
+	<section class="flex flex-col w-64 h-full px-4 py-8 overflow-y-auto md:mr-6">
+        <SideNavAdmin/>
+    </section>
+    
 
 	<section class="flex flex-col w-full h-screen p-6 ml-8 overflow-y-auto bg-gray-200 ">
         
-		<div class="overflow-x-auto relative shadow-md sm:rounded-lg h-full bg-white">
+		<div class="overflow-x-auto relative shadow-md sm:rounded-lg h-full bg-white mt-4">
 			
-				<table class="table table-normal w-full ">
+				<table class="table table-normal w-full bg-white ">
 				  	<thead>
 						<tr>
-					 		<th></th> 
+					 		<th>Client #</th> 
 					  		<th>Name</th> 
 					  		<th>Loan No.</th> 
-					  		<th>Business Name</th> 
+					  		<th>Loan Type</th> 
 						  	<th>location</th>
 							  <th></th> 
-					 		<th>Loan Type</th>
-						
+					 		<th>Release Date</th>
 							 <th></th> 
 						</tr>
 				 	</thead> 
-				  	<tbody>
 						<tr class="hover">
-							<th>1</th> 
+							<td>SPL 2022 0001</td> 
 							<td>Trishan Andrei</td> 
 							<td>No. 001</td> 
-							<td>sny printing</td> 
+							<td>New</td> 
 							<td>Canada</td> 
 							<td></td> 
-							<td>New</td>
-							<td class="flex gap-4 "><a href="#edit">
-							  </a><a href="#delete"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+							<td>12/12/2022</td>
+							<td>
+								<a href="#delete"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
 								<path fill-rule="evenodd" d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z" clip-rule="evenodd" />
 							  </svg>
 							  </a></td>
 						</tr>
 						<tr class="hover">
-							<th>2</th> 
+							<td>SPL 2022 0002</td> 
 							<td>Hart Hagerty</td> 
 							<td>No. 002</td> 
-							<td>Devine rapier inc</td> 
+							<td>Renewal</td> 
 							<td>United States</td> 
 							<td></td> 
-							<td>Renewal</td>
-                            <td class="flex gap-4"><a href="#edit">
-							  </a><a href="#delete"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+							<td>01/03/2023</td>
+                            <td>
+							<a href="#delete"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
 								<path fill-rule="evenodd" d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z" clip-rule="evenodd" />
 							  </svg>
 							  </a></td>
 						  </tr>
-					</tbody> 
+					
 				</table>	
 			</div>		
 		
