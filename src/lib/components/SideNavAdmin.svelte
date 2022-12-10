7c6dd7a452e96af3a9f2553419364a7fad73f5b4
Angel Paul Agnabo <darkangel22blazer@gmail.com>
2022-12-10 21:41:15 +0800

    added new tabs

--- src/lib/components/SideNavAdmin.svelte
+++ src/lib/components/SideNavAdmin.svelte
@@ -15,16 +15,24 @@
             </span>
             <span class="ml-2 text-sm tracking-wide truncate">General Reports</span>
           </a>
-          
-          <a href="/admin/clientprof" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500">
-            <span class="inline-flex justify-center items-center ml-4">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
-              </svg>
-            </span>
-            <span class="ml-2 text-sm tracking-wide truncate">Client Profile</span>
-          </a>
-          
+
+          <div class=" dropdown ">
+            <a href="#1" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+              <span class="inline-flex justify-center items-center ml-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+                  </svg>
+              </span>
+              <span class="ml-2 text-sm tracking-wide truncate">Client Management</span>
+              <svg sidebar-toggle-item class="w-6 h-6 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </a>
+            <ul class="dropdown-content menu p-2 shadow bg-base-100 w-full">
+              <li><a href="/admin/clientprof">Client Profile</a></li>
+              <li><a href="/admin/inquiries">Inquiries</a></li>
+              <li><a href="/admin/client_arrangement">Client Arrangement</a></li>
+            </ul>
+          </div>
+
           <a href="/admin/collectorsprof" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
             <span class="inline-flex justify-center items-center ml-4">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -34,38 +42,48 @@
             <span class="ml-2 text-sm tracking-wide truncate">Collectors Profile</span>
           </a>
       
-          <a href="/admin/inquiries" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+          <div class=" dropdown ">
+              <a href="#1" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                <span class="inline-flex justify-center items-center ml-4">
+                  <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path></svg>
+                </span>
+                <span class="ml-2 text-sm tracking-wide truncate">Transactions</span>
+                <svg sidebar-toggle-item class="w-6 h-6 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+              </a>
+              <ul class="dropdown-content menu p-2 shadow bg-base-100 w-full">
+                <li><a href="/admin/loan_transactions">Loan Processing</a></li>
+                <li><a href="#1">Payments</a></li>
+                <li><a href="#1">Payment Transfer</a></li>
+                <li><a href="#1">Adjustments</a></li>
+                <li><a href="#1">PR series</a></li>
+                <li><a href="#1">Cashier Entry</a></li>
+              </ul>
+          </div>
+
+          <a href="#promotions" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
             <span class="inline-flex justify-center items-center ml-4">
               <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path></svg>
             </span>
-            <span class="ml-2 text-sm tracking-wide truncate">Inquiries</span>
+            <span class="ml-2 text-sm tracking-wide truncate">Promotions</span>
             
           </a>
-      <div class=" dropdown ">
-          <a href="#1" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
-            <span class="inline-flex justify-center items-center ml-4">
-              <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path></svg>
-            </span>
-            <span class="ml-2 text-sm tracking-wide truncate">Transactions</span>
-            <svg sidebar-toggle-item class="w-6 h-6 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-          </a>
-          <ul class="dropdown-content menu p-2 shadow bg-base-100 w-full">
-            <li><a href="/admin/loan_transactions">Loan Processing</a></li>
-            <li><a href="#1">Payments</a></li>
-            <li><a href="#1">Payment Transfer</a></li>
-            <li><a href="#1">Adjustments</a></li>
-            <li><a href="#1">PR series</a></li>
-            <li><a href="#1">Cashier Entry</a></li>
-          </ul>
-      </div>
 
-      <a href="#promotions" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
-        <span class="inline-flex justify-center items-center ml-4">
-          <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path></svg>
-        </span>
-        <span class="ml-2 text-sm tracking-wide truncate">Promotions</span>
-        
-      </a>
+          <div class=" dropdown ">
+            <a href="#1" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+              <span class="inline-flex justify-center items-center ml-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+                  </svg>
+              </span>
+              <span class="ml-2 text-sm tracking-wide truncate">System Administration</span>
+              <svg sidebar-toggle-item class="w-6 h-6 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </a>
+            <ul class="dropdown-content menu p-2 shadow bg-base-100 w-full">
+              <li><a href="/admin/users">Users</a></li>
+              <li><a href="/admin/change_password">Change Password</a></li>
+              <li><a href="/admin/db_backup">DB Backup</a></li>
+            </ul>
+          </div>
 
         <!-- logout hiwalay ko lang --> 
           <a href="#1" class="absolute bottom-0 left-0 flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent">
---
+++ src/routes/admin/client_arrangement/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import SideNavAdmin from "../../../lib/components/SideNavAdmin.svelte";
+</script>
+<main class=" flex">
+
+	<section class="flex flex-col w-64 h-full px-4 py-8 overflow-y-auto">
+		<SideNavAdmin/>
+	</section>
+	
+	<section class="flex flex-col w-full h-screen p-6 ml-8 overflow-y-auto bg-gray-200">
+		<div class=" w-full flex flex-col p-3 bg-gray-200 h-full">
+			<div class="bg-white mb-3 p-2 text-2xl">
+				Client Arrangement
+			</div>
+		</div>
+	</section>	
+</main>
