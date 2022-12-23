14347db7bbc0664f7453ce5124b6d27bf77e55c0
Angel Paul Agnabo <darkangel22blazer@gmail.com>
2022-12-23 14:59:15 +0800

    added delte button in loan processing

--- src/routes/(admin-side)/admin/transactions/loan_processing/+page.svelte
+++ src/routes/(admin-side)/admin/transactions/loan_processing/+page.svelte
@@ -11,13 +11,18 @@
 	<section class="flex flex-col w-full h-screen p-6 ml-8 overflow-y-auto bg-gray-200">
         <div class="flex items-center p-4 shadow-md sm:rounded-lg h-10 bg-white gap-4">
             Loan Processing 
-            <label for="my-modal" class="absolute right-16 font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                <div class=" btn btn-ghost bg-gray-200 btn-xs sm:btn-2xs md:btn-xs lg:btn-sm hover:bg-green-300">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" class="w-5 h-5">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>
-                    </div>
-
-            </label>
+            <div class="absolute right-16">
+                <label for="my-modal" class="font-medium text-blue-600 p-2 dark:text-blue-500 hover:underline">
+                    <div class=" btn btn-ghost bg-gray-200 btn-xs sm:btn-2xs md:btn-xs lg:btn-sm hover:bg-green-300">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" class="w-5 h-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>
+                        </div>
+                </label>
+                <button for="my-modal" class=" btn btn-ghost bg-gray-200 btn-xs sm:btn-2xs md:btn-xs lg:btn-sm hover:bg-red-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /></svg>   
+                </button>
+            </div>
         </div>
         
 		
