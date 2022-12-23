bd6bb0eb379707d42bcc985e4193124c2f33ab61
Francis Sanchez <sanchez.francispup@gmail.com>
2022-12-23 17:59:57 +0800

    Dynamic Page added in Inquiries

---
+++ src/routes/(admin-side)/admin/clientmanage/inquiries/[inquiryId]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    export let data; 
+
+    const {inquiryId} = data
+    console.log(inquiryId)
+</script>
+
+<a href="/admin/clientmanage/inquiries/">Back</a>
+<h1>{inquiryId}</h1>
