e29bf0954ab3112d89917adae94a9b3496eeca16
Angel Paul Agnabo <darkangel22blazer@gmail.com>
2022-12-26 12:01:05 +0800

    admin user add button modal created

--- src/routes/(admin-side)/admin/systemad/users/+page.svelte
+++ src/routes/(admin-side)/admin/systemad/users/+page.svelte
@@ -1,13 +1,13 @@
 	<div class="flex items-center p-4 shadow-md sm:rounded-lg h-10 bg-white gap-4">
 		Admin Users
-		<div class="flex items-center absolute right-10 p-4 shadow-md sm:rounded-lg h-10 bg-white gap-4">
+		<div class="flex items-center absolute right-10 sm:rounded-lg bg-white gap-4">
 			
-			<label for="my-modal" class=" btn btn-ghost bg-gray-200 btn-xs sm:btn-2xs md:btn-xs lg:btn-sm hover:bg-green-300">
+			<label for="add" class=" btn btn-ghost bg-gray-200 btn-xs sm:btn-2xs md:btn-xs lg:btn-sm hover:bg-green-300">
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" class="w-5 h-5">
 					<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>
 			</label>
 			
-			<button for="my-modal" class=" btn btn-ghost bg-gray-200 btn-xs sm:btn-2xs md:btn-xs lg:btn-sm hover:bg-red-300">
+			<button class=" btn btn-ghost bg-gray-200 btn-xs sm:btn-2xs md:btn-xs lg:btn-sm hover:bg-red-300">
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 					<path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /></svg>   
 			</button>
@@ -135,6 +135,39 @@
 			<div class="modal-action">
 				<label for="my-modal" class="btn border-transparent bg-green-600">Save</label>
 				<label for="my-modal" class="btn border-transparent bg-red-600">Cancel</label>
+			</div>
+		</div>
+	</div>
+
+	<input type="checkbox" id="add" class="modal-toggle" />
+	<div class="modal">
+		<div class="modal-box">
+
+			<!-- Modal -->
+			<form action="#" class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+				<!-- Modal header -->
+				<div class="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
+					<h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+						Add User
+					</h3>
+				</div>
+				<!-- Modal body -->
+				<div class="p-6 space-y-6">
+					<div class="grid grid-cols-6 gap-6">
+						<div class="col-span-6 sm:col-span-3">
+							<label for="Email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+							<input type="text" name="Email" id="Email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="adminuser01@gmail.com" required="">
+						</div>
+						<div class="col-span-6 sm:col-span-3">
+							<label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+							<input type="text" name="password" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="password" required="">
+						</div>
+						
+				</div>
+			</form>
+			<div class="modal-action">
+				<label for="add" class="btn border-transparent bg-green-600">Save</label>
+				<label for="add" class="btn border-transparent bg-red-600">Cancel</label>
 			</div>
 		</div>
 	</div>
