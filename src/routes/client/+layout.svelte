e5461b088af0ea22fb0e2f19ea85837311448cb9
Angel Paul Agnabo <darkangel22blazer@gmail.com>
2023-01-16 23:12:06 +0800

    added client signin ang log out

--- src/routes/client/+layout.svelte
+++ src/routes/client/+layout.svelte
@@ -1,11 +1,42 @@
 <script>
 	import '../../app.postcss';
 	import SideNav from '$lib/components/SideNav.svelte';
+    import {userStore} from '$lib/store.js';
+    import {goto} from '$app/navigation';
+    import {getDoc,doc} from 'firebase/firestore';
+    import {db} from '$lib/firebase/client.js';
+    import {signOut} from 'firebase/auth'
+    let show = false;
+
+    async function checkIfAdmin(_){
+        if($userStore===undefined){
+            return;
+        }
+        if($userStore===null){
+           await goto('/');
+           return;
+        }
+
+      const snapshot = await getDoc(doc(db,'userinfo',$userStore.uid))
+      const isAdmin = snapshot.get('admin')
+
+        if(isAdmin===undefined || isAdmin===false){
+            show = true;
+            return;
+        
+        }
+        await goto('/');
+    }
+
+    $: checkIfAdmin($userStore)
+    
 </script>
 
+{#if show}
 <main class=" flex">
 	<section class="flex flex-col w-64 h-full px-4 py-8 overflow-y-auto">
 		<SideNav/>
 	</section>
 	<slot />
 </main>
+{/if}
