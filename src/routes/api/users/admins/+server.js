987b5f19df6b43785a7ed819c515fc6b741772e5
Francis Sanchez <sanchez.francispup@gmail.com>
2023-01-19 00:06:30 +0800

    nothing here

--- src/routes/api/users/admins/+server.js
+++ src/routes/api/users/admins/+server.js
@@ -10,7 +10,6 @@ export async function GET() {
 export async function POST({request}) {
     const addUserInput = await request.json();
     const userRecord = await adminAuth.createUser({ email:addUserInput.email, password:addUserInput.password, displayName:addUserInput.name});
-    // await adminAuth.updateUser(userRecord.uid,{displayName:addUserInput.name})
     return json(userRecord);
 
 };
