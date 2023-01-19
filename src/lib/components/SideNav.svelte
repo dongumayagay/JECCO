6b7add89d68792ec023e7a2d0d18a5a1767ed460
Francis Sanchez <sanchez.francispup@gmail.com>
2023-01-19 12:30:51 +0800

    clean up 2.0

--- src/lib/components/SideNav.svelte
+++ src/lib/components/SideNav.svelte
@@ -21,14 +21,14 @@
     </div>
     <div class="overflow-y-auto overflow-x-hidden flex-grow">
       <div class="flex flex-col py-4 space-y-1">
-        <a href="/client" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+        <!-- <a href="/client" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
           <span class="inline-flex justify-center items-center ml-4">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z" />
             </svg>
           </span>
           <span class="ml-2 text-sm tracking-wide truncate">News Feed</span>
-        </a>
+        </a> -->
           <a href="/client/payment_history" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
             <span class="inline-flex justify-center items-center ml-4">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
--- src/routes/client/+page.svelte
+++ src/routes/client/+page.svelte
@@ -1,4 +1,4 @@
-<section class="flex flex-col w-full h-screen p-6 ml-8 overflow-y-auto bg-gray-200">
+<!-- <section class="flex flex-col w-full h-screen p-6 ml-8 overflow-y-auto bg-gray-200">
 	<div class="bg-white p-2 text-2xl w-full h-full flex flex-col">
 		News Feed
 	</div>
@@ -7,6 +7,42 @@
 			<img class="my-2" src="https://25174313.fs1.hubspotusercontent-eu1.net/hubfs/25174313/assets_moneymax/Blog_Featured_Image_l_How_a_Personal_Loan_is_Calculated.png" alt="promo img">
 			<img class="my-2" src="https://25174313.fs1.hubspotusercontent-eu1.net/hub/25174313/hubfs/assets_moneymax/Best_Personal_Loans.png?width=680&name=Best_Personal_Loans.png" alt="promo 2">
 			<img class="my-2" src="https://25174313.fs1.hubspotusercontent-eu1.net/hub/25174313/hubfs/Low-Interest_Personal_Loans.png?width=680&name=Low-Interest_Personal_Loans.png" alt="promo 2">
+		</div>
+	</div>
+</section>	 -->
+
+<section class="flex flex-col w-full h-screen p-6 ml-8 overflow-y-auto bg-gray-200">
+	<div class=" w-full flex flex-col p-3 bg-gray-200 h-full">
+		<div class="bg-white mb-3 p-2 text-2xl">
+			Payment History
+		</div>
+			<div class="bg-white mb-3 p-2 h-full">
+			<div class="overflow-x-auto">
+				<table class="table table-normal w-full">
+					<thead>
+						<tr class="hover">
+							<th></th> 
+							<th>Loan Status</th> 
+							<th>Amount Paid</th> 
+							<th>Date Paid</th> 								
+						</tr>
+					</thead> 
+					<tbody>
+						<tr class="hover">
+							<th>1</th> 
+							<td>Active</td> 
+							<td>5,000.00 php</td> 
+							<td>12/16/2020</td> 
+						</tr>
+						<tr class="hover">
+							<th>2</th> 
+							<td>Active</td> 
+							<td>10,000.00 php</td> 
+							<td>11/17/2020</td> 
+						</tr>
+					</tbody> 
+				</table>	
+			</div>		
 		</div>
 	</div>
 </section>	
