5dd499ca60c10eac98f63ed631f521dbe7fa1bc3
Francis Sanchez <sanchez.francispup@gmail.com>
2023-01-22 21:40:08 +0800

    [FIXED] overlap sidebar bg

--- src/routes/(admin-side)/kanrisha/+page.svelte
+++ src/routes/(admin-side)/kanrisha/+page.svelte
@@ -4,6 +4,13 @@
     
 </script>
 
-    <div class="flex bg-white w-11/12 max-w-3xl p-4 ">
+<div class="flex gap-6">
+    <div class="flex flex-1 bg-white w-11/12 max-w-3xl p-4 ">
         <canvas id="myChart"> <Chart/> </canvas>
     </div>
+
+    <div class="flex flex-1 bg-gray-100 w-11/12 h-screen p-4 ">
+
+
+    </div>
+</div>
