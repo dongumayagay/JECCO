9cc042998ad19af317df6cf2cfaad12bbb321cea
Francis Sanchez <sanchez.francispup@gmail.com>
2023-01-25 01:49:09 +0800

    [fixed] api syntax error / newfields added

--- src/lib/components/AddClientProfile.svelte
+++ src/lib/components/AddClientProfile.svelte
@@ -16,19 +16,25 @@
             houseNo:cliInfo.houseNo,
             barangay:cliInfo.barangay,
             municipality:cliInfo.municipality,
-            province:cliInfo.province
+            province:cliInfo.province,
         }
     }
 
     function resetAddUserInput(){
 		addUserInput = {
             username:'',
+            email:'',
             password:'',
             confirmPassword:'',
             barangay:'',
             houseNo:'',
             municipality:'',
             province:'',
+            acctOfficer:'',
+            crdtInv:'',
+            approved:'',
+            released:'',
+            collector:'',
             firstname:cliInfo.firstname,
             lastname:cliInfo.lastname,
             email:cliInfo.email,
@@ -36,7 +42,9 @@
             barangay:cliInfo.barangay,
             houseNo:cliInfo.houseNo,
             municipality:cliInfo.municipality,
-            province:cliInfo.province
+            province:cliInfo.province,
+            
+            
 	    } 
 	}
 
@@ -47,15 +55,11 @@
 		}
 		try {
 
-			const response = await fetch('/api/users/admins',{method:'POST',
+			const response = await fetch('/api/clients',{method:'POST',
 			body: JSON.stringify({
                 name:addUserInput.firstname + ' ' + addUserInput.lastname,
-                email:addUserInput.username,
-                password:addUserInput.password
-			})})
-			const userRecord = await response.json()
-			const docRef = await setDoc(doc(db, "clientinfo", userRecord.uid), {
                 username:addUserInput.username,
+                password:addUserInput.password,
                 firstname:addUserInput.firstname,
                 lastname:addUserInput.lastname,
                 email:addUserInput.email,
@@ -63,8 +67,14 @@
                 barangay:addUserInput.barangay,
                 houseNo:addUserInput.houseNo,
                 municipality:addUserInput.municipality,
-                province:addUserInput.province
-        	});
+                province:addUserInput.province,
+                acctOfficer:addUserInput.acctOfficer,
+                crdtInv:addUserInput.crdtInv,
+                approved:addUserInput.approved,
+                released:addUserInput.released,
+                collector:addUserInput.collector
+			})})
+			
 			
 		} catch (error) {
 			console.log(error)
@@ -130,11 +140,31 @@
                     </div>
                     <div class="">
                         <label for="muni" class="block mb-2 text-sm font-medium  ">Municipality</label>
-                        <input type="text"  bind:value={addUserInput.municipality} class="shadow-sm  border overflow-y-auto text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Municipality" required="">
+                        <input type="text"  bind:value={addUserInput.municipality} class="shadow-sm  border overflow-y-auto text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Municipality" required>
                     </div>
                     <div class="">
                         <label for="prov" class="block mb-2 text-sm font-medium  ">Province</label>
-                        <input type="text" bind:value={addUserInput.province} class="shadow-sm  border overflow-y-auto text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500 " placeholder="Province" required="">
+                        <input type="text" bind:value={addUserInput.province} class="shadow-sm  border overflow-y-auto text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500 " placeholder="Province" required>
+                    </div>
+                    <div class="">
+                        <label for="prov" class="block mb-2 text-sm font-medium  ">Account Officer:</label>
+                        <input type="text" bind:value={addUserInput.acctOfficer} class="shadow-sm  border overflow-y-auto text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500 " placeholder="Account Officer" required>
+                    </div>
+                    <div class="">
+                        <label for="prov" class="block mb-2 text-sm font-medium  ">Credit Investigated By:</label>
+                        <input type="text" bind:value={addUserInput.crdtInv} class="shadow-sm  border overflow-y-auto text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500 " placeholder="Credit Investigated By" required>
+                    </div>
+                    <div class="">
+                        <label for="prov" class="block mb-2 text-sm font-medium  ">Approved By:</label>
+                        <input type="text" bind:value={addUserInput.approved} class="shadow-sm  border overflow-y-auto text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500 " placeholder="Approved By" required>
+                    </div>
+                    <div class="">
+                        <label for="prov" class="block mb-2 text-sm font-medium  ">Released By:</label>
+                        <input type="text" bind:value={addUserInput.released} class="shadow-sm  border overflow-y-auto text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500 " placeholder="Released By" required>
+                    </div>
+                    <div class="">
+                        <label for="prov" class="block mb-2 text-sm font-medium  ">Collector Assigned:</label>
+                        <input type="text" bind:value={addUserInput.collector} class="shadow-sm  border overflow-y-auto text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500 " placeholder="Collector Assigned" required>
                     </div>
                 </div>
             </div>
--- src/routes/api/clients/+server.js
+++ src/routes/api/clients/+server.js
@@ -1,6 +1,8 @@
 import {json} from '@sveltejs/kit';
-import { collection, getDocs} from 'firebase/firestore';
+import { collection, getDocs, setDoc, doc} from 'firebase/firestore';
 import {db} from '$lib/firebase/client.js';
+import { adminAuth } from '$lib/firebase/admin.server';
+
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET() {
@@ -12,4 +14,30 @@ export async function GET() {
     });
 
     return json(clients);
+};
+
+
+/** @type {import('./$types').RequestHandler} */
+export async function POST({request}) {
+    const addUserInput = await request.json();
+    const userRecord = await adminAuth.createUser({ email:addUserInput.username, password:addUserInput.password, displayName:addUserInput.name});
+    const docRef = await setDoc(doc(db, "clientinfo", userRecord.uid), {
+        username:addUserInput.username,
+        firstname:addUserInput.firstname,
+        lastname:addUserInput.lastname,
+        email:addUserInput.email,
+        number:addUserInput.number,
+        barangay:addUserInput.barangay,
+        houseNo:addUserInput.houseNo,
+        municipality:addUserInput.municipality,
+        province:addUserInput.province,
+        acctOfficer:addUserInput.acctOfficer,
+        crdtInv:addUserInput.crdtInv,
+        approved:addUserInput.approved,
+        released:addUserInput.released,
+        collector:addUserInput.collector
+    });
+
+    return json(docRef);
+
 };
