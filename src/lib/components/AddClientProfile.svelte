506b7b774fdffcdadf658aff4becf946c67f2cdf
Francis Sanchez <sanchez.francispup@gmail.com>
2023-02-12 19:01:20 +0800

    input Validations added

--- src/lib/components/AddClientProfile.svelte
+++ src/lib/components/AddClientProfile.svelte
@@ -2,10 +2,12 @@
     let addModal = false;
     let cliInfo = []
     let addUserInput = {}
-    let prefix = "SPL"
+    let prefix = "SPL";
     let count = 1
     let ctrlNumber = "000000"
-    let thisClientNumber=""
+    let thisClientNumber="";
+    let errorPassword = '';
+    let showPassword = false;
 
     export async function clientInfo(infoClient,clients){
         cliInfo = infoClient
@@ -81,6 +83,18 @@
 		addModal = false
 	}
 
+    function validatePassword(){
+        if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(password)) {
+            errorPassword = 'Password must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters';
+        } else {
+            errorPassword = '';
+        }
+
+    }
+
+    function toggleShowPassword(){
+        showPassword = !showPassword;
+    }
 </script>
 
 <input type="checkbox" bind:checked={addModal} id="add" class="modal-toggle" />
@@ -88,71 +102,83 @@
     <div class="modal-box max-w-xl">
 
         <!-- Modal -->
-        <form class="relative bg-white rounded shadow dark:bg-gray-700 " on:submit={addUser}>
+        <form class="relative bg-white rounded shadow" on:submit={addUser}>
             <!-- Modal header -->
-            <div class="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
-                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+            <div class="p-4 border-b ">
+                <h3 class="text-xl font-semibold">
                     Add Client Profile
                 </h3>
             </div>
             <!-- Modal body -->
-            <div class="p-6 space-y-6">
+            <div class="p-6">
                 <div class="grid grid-cols-2 gap-6">
-                    <div class="">
-                        <label for="first-name" class="block mb-2 text-sm font-medium dark:text-white ">Client Number</label>
-                        <input type="text" disabled bind:value={addUserInput.clientNumber} class="shadow-sm border text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Client Number" required>
+                    <div>
+                        <label for="client-num" class="mb-2 text-sm font-medium">Client Number</label>
+                        <input type="text" id="client-num" disabled bind:value={addUserInput.clientNumber} class="shadow border text-sm rounded-lg w-56 p-2.5" placeholder="Client Number">
                     </div>
-                    <div class="">
-                        <label for="first-name" class="block mb-2 text-sm font-medium dark:text-white ">First Name</label>
-                        <input type="text" bind:value={addUserInput.firstname} class="shadow-sm border text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="First Name" required>
+                    <div>
+                        <label for="first-name" class="mb-2 text-sm font-medium">First Name</label>
+                        <input type="text" id="first-name" bind:value={addUserInput.firstname} class="shadow-sm border text-sm capitalize rounded-lg w-56 p-2.5" placeholder="First Name" maxlength="30" minlength="2" required>
                     </div>
-                    <div class>
-                        <label for="last-name" class="block mb-2 text-sm font-medium dark:text-white ">Last Name</label>
-                        <input type="text" bind:value={addUserInput.lastname} class="shadow-sm border text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Last Name" required>
+                    <div>
+                        <label for="last-name" class="mb-2 text-sm font-medium">Last Name</label>
+                        <input type="text" id="last-name" bind:value={addUserInput.lastname} class="shadow-sm border text-sm capitalize rounded-lg w-56 p-2.5" placeholder="Last Name" maxlength="30" minlength="2" required>
                     </div>
-                    <div class="">
-                        <label for="username" class="block mb-2 text-sm font-medium dark:text-white ">Username</label>
-                        <input type="email" bind:value={addUserInput.username} class="shadow-sm  border text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" required>
+                    <div>
+                        <label for="username" class="mb-2 text-sm font-medium">Username</label>
+                        <input type="email" id="username" bind:value={addUserInput.username} class="shadow-sm  border text-sm rounded-lg w-56 p-2.5" placeholder="Email" maxlength="30" minlength="5" required>
                     </div>
-                    <div class="">
-						<label for="password" class="block mb-2 text-sm font-medium dark:text-white">Password</label>
-						<input type="password" bind:value={addUserInput.password} class="shadow-sm border text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Password" required>
+                    <div>
+                        <label for="password" class="mb-2 text-sm font-medium">Password</label>
+                        {#if showPassword}
+						<input type="text" id="password" bind:value={addUserInput.password} on:input={validatePassword} class="shadow-sm border text-sm rounded-lg w-56 p-2.5" placeholder="Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
+                        {:else}
+                        <input id="password" type="password" bind:value={addUserInput.password} on:input={validatePassword} class="shadow-sm border text-sm rounded-lg w-56 p-2.5" placeholder="Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
+                        {/if}
+                        <div class=" relative text-sm mt-3 font-mono ">
+                            <input type="checkbox" class="checkbox checkbox-xs" on:click={toggleShowPassword}>
+                                {showPassword ? "Show" : "Show"} password
+                        </div>
 					</div>
-					<div class="">
-						<label for="confirmPassword" class="block mb-2 text-sm font-medium dark:text-white">Confirm Password</label>
-						<input type="password" bind:value={addUserInput.confirmPassword} class="shadow-sm border text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Confirm password" required>
+					<div>
+						<label for="confirm-pass" class="mb-2 text-sm font-medium dark:text-white">Confirm Password</label>
+                        {#if showPassword}
+						<input type="text" id="confirm-pass" bind:value={addUserInput.confirmPassword} class="shadow-sm border text-sm rounded-lg w-56 p-2.5" placeholder="Confirm password" required>
+                        {:else}
+                        <input type="password" id="confirm-pass" bind:value={addUserInput.confirmPassword} class="shadow-sm border text-sm rounded-lg w-56 p-2.5" placeholder="Confirm password" required>
+                        {/if}
 					</div> 
-                    <div class="">
-                        <label for="email" class="block mb-2 text-sm font-medium dark:text-white ">Email</label>
-                        <input type="email" bind:value={addUserInput.email} class="shadow-sm  border text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" required>
+                    <div>
+                        <label for="email" class="mb-2 text-sm font-medium">Email</label>
+                        <input type="email" id="email" bind:value={addUserInput.email} class="shadow-sm  border text-sm rounded-lg w-56 p-2.5" placeholder="Email" required>
                     </div>
-                    <div class>
-                        <label for="last-name" class="block mb-2 text-sm font-medium dark:text-white ">Co Maker</label>
-                        <input type="text" bind:value={addUserInput.coMaker} class="shadow-sm border text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Co Maker" required>
+                    <div>
+                        <label for="co-maker" class="mb-2 text-sm font-medium">Co Maker</label>
+                        <input type="text" id="co-maker" bind:value={addUserInput.coMaker} class="shadow-sm border text-sm capitalize rounded-lg w-56 p-2.5" placeholder="Co Maker" required>
                     </div>
-                    <div class="">
-                        <label for="contact-number" class="block mb-2 text-sm font-medium dark:text-white ">Contact Number</label>
-                        <input type="text" bind:value={addUserInput.number} class="shadow-sm  border text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Contact Info" required>
+                    <div>
+                        <label for="contact-number" class="mb-2 text-sm font-medium">Contact Number</label>
+                        <input type="text" id="contact-number" bind:value={addUserInput.number} class="shadow-sm  border text-sm rounded-lg w-56 p-2.5" placeholder="Contact Info" required>
                     </div>
-                    <div class="">
-                        <label for="house" class="block mb-2 text-sm font-medium dark:text-white ">House No.</label>
-                        <input type="text" bind:value={addUserInput.houseNo} class="shadow-sm  border overflow-y-auto text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="House No." required>
+                    <div>
+                        <label for="house" class="mb-2 text-sm font-medium">House No.</label>
+                        <input type="text" id="house" bind:value={addUserInput.houseNo} class="shadow-sm  border overflow-y-auto text-sm rounded-lg w-56 p-2.5" placeholder="House No." required>
                     </div>
-                    <div class="">
-                        <label for="brgy" class="block mb-2 text-sm font-medium dark:text-white ">Barangay</label>
-                        <input type="text" bind:value={addUserInput.barangay} class="shadow-sm  border overflow-y-auto text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Barangay" required>
+                    <div>
+                        <label for="brgy" class="mb-2 text-sm font-medium">Barangay</label>
+                        <input type="text" id="brgy" bind:value={addUserInput.barangay} class="shadow-sm  border overflow-y-auto text-sm capitalize rounded-lg w-56 p-2.5" placeholder="Barangay" required>
                     </div>
-                    <div class="">
-                        <label for="muni" class="block mb-2 text-sm font-medium dark:text-white ">Municipality</label>
-                        <input type="text"  bind:value={addUserInput.municipality} class="shadow-sm  border overflow-y-auto text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Municipality" required>
+                    <div>
+                        <label for="muni" class="mb-2 text-sm font-medium">Municipality</label>
+                        <input type="text" id="muni" bind:value={addUserInput.municipality} class="shadow-sm  border overflow-y-auto text-sm capitalize rounded-lg w-56 p-2.5" placeholder="Municipality" required>
                     </div>
-                    <div class="">
-                        <label for="prov" class="block mb-2 text-sm font-medium dark:text-white ">Province</label>
-                        <input type="text" bind:value={addUserInput.province} class="shadow-sm  border overflow-y-auto text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500 " placeholder="Province" required>
+                    <div>
+                        <label for="prov" class="mb-2 text-sm font-medium">Province</label>
+                        <input type="text" id="prov" bind:value={addUserInput.province} class="shadow-sm  border overflow-y-auto text-sm rounded-lg w-56 p-2.5 " placeholder="Province" required>
                     </div>
-                    <div class="">
-                        <label for="date-created" class="block mb-2 text-sm font-medium dark:text-white ">Date Created</label>
-                        <input type="date" bind:value={addUserInput.dateCreated} class="shadow-sm  border overflow-y-auto text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-56 p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="dd/mm/yyyy" required>
+                    <div>
+                        <label for="date-created" class="mb-2 text-sm font-medium">Date Created</label>
+                        <input type="date" id="date-created" bind:value={addUserInput.dateCreated} class="shadow-sm  border overflow-y-auto text-sm rounded-lg w-56 p-2.5" placeholder="dd/mm/yyyy" required>
                     </div>
                 </div>
             </div>
