c71f8e36a68ac585a3367b200d21f71fa561fc6a
Francis Sanchez <sanchez.francispup@gmail.com>
2023-03-07 02:17:23 +0800

    generate report tab removed

--- src/lib/components/SideNavAdmin.svelte
+++ src/lib/components/SideNavAdmin.svelte
@@ -81,14 +81,6 @@
               </span>
               <span class="ml-2 text-sm tracking-wide truncate">Admin User</span>
             </a>
-            <a href="/kanrisha/generate_reports" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
-              <span class="inline-flex justify-center items-center ml-4">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-                </svg>                
-              </span>
-              <span class="ml-2 text-sm tracking-wide truncate">Generate Reports</span>
-            </a>
             
         <!-- logout --> 
           <button class="absolute bottom-0 left-0 flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent" on:click={logout}>
