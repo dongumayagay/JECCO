35c1265538a7d96f494802c6c8f0157b43f9c4dc
Francis Sanchez <sanchez.francispup@gmail.com>
2023-03-07 21:26:06 +0800

    client side dashboard

--- src/routes/client/+page.svelte
+++ src/routes/client/+page.svelte
@@ -1,5 +1,24 @@
 <script>
   import {userStore} from '$lib/store.js'
+  import { doc, getDoc } from 'firebase/firestore';
+  import {db} from '$lib/firebase/client.js';
+
+  let clientData = [];
+
+  async function getClient() {
+    const userId = $userStore.uid;
+    const clientRef = doc(db, "clientinfo", userId);
+    const clientSnapshot = await getDoc(clientRef);
+
+    if (clientSnapshot.exists()) {
+       clientData = clientSnapshot.data();
+      return clientData;
+    } else {
+      alert("No Document Found");
+    }
+  }
+
+  getClient();
   
 </script>
 
@@ -9,15 +28,26 @@
 
 
   
-  <section class="flex flex-col flex-1 bg-white rounded-lg" >
+  <section class="flex flex-col h-screen bg-white rounded-lg" >
     <!-- <h1 class="p-5 font-bold text-xl">Client Dashboard</h1>
     <hr/> -->
-    <div class=" flex flex-col w-full h-1/2 p-6 gap-6">
-      <div class="">
+    <div class=" flex flex-col w-full p-2 gapx-6">
+      <div class="flex w-full h-14 items-center">
         {#if $userStore }
-        <h2 class="font-semibold text-4xl">Welcome {$userStore.displayName}!</h2>
+        <h2 class="font-semibold text-xl pl-6 max-sm:text-lg">{$userStore.displayName}</h2>
         {/if}
+        <span class="font-semibold text-xl pl-2 max-sm:text-lg">({clientData.clientNumber})</span>        
+      </div>
+      <hr/>
+      <div class=" flex flex-col w-full h-32 p-6 mt-3 rounded-md gap-4 max-sm:gap-2 font-semibold justify-center shadow-md">
+          <span>Email: {clientData.email}</span>
+          <span>Address: {clientData.houseNo + ', ' + clientData.barangay + ', ' + clientData.municipality + ', ' + clientData.province}</span>
+          <span>Loan Status: {clientData.status}</span>
       </div>
     </div>
 
+    <div class="flex items-center justify-center mt-3">
+      <img src="/finance.gif" alt="">
+
+    </div>
   </section>
