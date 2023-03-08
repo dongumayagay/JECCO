3d6a40296c6baa87d3c50272906bbfb85c3cb6dd
Francis Sanchez <sanchez.francispup@gmail.com>
2023-03-08 23:17:31 +0800

    disable renewal when loan is ongoing

--- src/lib/components/SideNav.svelte
+++ src/lib/components/SideNav.svelte
@@ -3,13 +3,45 @@
   import {auth} from '$lib/firebase/client.js'
   import {signOut} from 'firebase/auth'
   import {userStore} from '$lib/store.js'
+  import { collection, onSnapshot, query, where, orderBy } from 'firebase/firestore';
+  import {db} from '$lib/firebase/client.js';
 
-  let sidebarVisible = true; 
+  let renewal = false;
 
   async function logout(){
       await signOut(auth)
       await goto('/')
   }
+
+  let loans = []
+	
+	async function userLoans() {
+        loans = []
+
+        const q = query(collection(db, 'loanprocess'), where("owner", "==", $userStore.uid));
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            loans = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+            // console.log(loans[1]);
+            if(loans[1].status !== "Ongoing"){
+                 renewal = true;
+            }
+        });
+        
+        
+        return () => unsubscribe();
+        
+    }
+
+	userLoans();
+  
+  function handleClick(event) {
+    if (loans[1].status === "Ongoing") {
+      event.preventDefault();
+    }
+  }
+    
+
+
 
 </script>
 
@@ -53,7 +85,7 @@
             </span>
             <span class="ml-2 text-sm tracking-wide max-sm:hidden">Loans</span>
           </a>
-          <a href="/application" class="relative flex items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 hover:border-indigo-500">
+          <a href="/application" on:click={handleClick} class={renewal ? 'relative flex items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 hover:border-indigo-500' : 'relative flex items-center h-11 text-gray-200 disabled cursor-not-allowed'}>
             <span class="flex justify-center items-center">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75" />
