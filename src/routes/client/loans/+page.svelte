988397b1515ee6e1bfadddd3d1653adb720fc41b
FRANCIS_SANCHEZ <sanchez.francispup@gmail.com>
2023-07-15 03:51:03 +0800

    client side pagination added

--- src/routes/client/loans/+page.svelte
+++ src/routes/client/loans/+page.svelte
@@ -4,8 +4,15 @@
 	import { collection, onSnapshot, query, where, orderBy } from 'firebase/firestore';
 	import { jsPDF } from "jspdf";
     import autoTable from 'jspdf-autotable'
+	import { onMount, afterUpdate } from 'svelte';
 	
 	let loans = []
+	let currentPage = 1;
+    let itemsPerPage = 12;
+
+    let totalItems = 0;
+    let totalPages = 0;
+    let displayedItems = [];
 	
 	async function userLoans() {
         loans = []
@@ -13,12 +20,32 @@
         const q = query(collection(db, 'loanprocess'), where("owner", "==", $userStore.uid), orderBy("numberOfLoan", "desc") );
         const unsubscribe = onSnapshot(q, (querySnapshot) => {
             loans = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+			totalItems = loans.length;
+            totalPages = Math.ceil(totalItems / itemsPerPage);
+            updateDisplayedItems();
         });
         return () => unsubscribe();
         
     }
 
-	userLoans();
+	function updateDisplayedItems() {
+        let startIndex = (currentPage - 1) * itemsPerPage;
+        let endIndex = startIndex + itemsPerPage;
+        displayedItems = loans.slice(startIndex, endIndex);
+    }
+
+    function goToPage(pageNumber) {
+        currentPage = pageNumber;
+        updateDisplayedItems();
+    }
+
+    onMount(() => {
+        userLoans();
+    });
+
+    afterUpdate(() => {
+        updateDisplayedItems();
+    });
 
 	async function generateLoans(){
 	 const paymentHistory = new jsPDF();
@@ -67,7 +94,7 @@
 						<th scope="col" class="px-6">STATUS</th> 
 					</tr>
 				</thead>
-				{#each loans as loan}
+				{#each displayedItems as loan}
 					<tr>
 						<td class="px-6">
 							<p>{loan.numberOfLoan}</p>
@@ -100,5 +127,38 @@
 				{/each}
 			</table>	
 		</div>
+
+		<!-- Pagination -->
+<div class="mt-4 flex justify-center">
+    <button class="cursor-pointer px-2 py-1 rounded hover:bg-gray-200" disabled={currentPage === 1} on:click={() => goToPage(1)}>
+      First
+    </button>
+    <button class="cursor-pointer px-2 py-1 rounded hover:bg-gray-200" disabled={currentPage === 1} on:click={() => goToPage(currentPage - 1)}>
+      Previous
+    </button>
+
+    <!-- {#if currentPage > 3}
+      <span class="cursor-pointer px-2 py-1 rounded hover:bg-gray-200">...</span>
+    {/if} -->
+
+    <!-- {#each Array.from({ length: totalPages }, (_, i) => i + 1) as page}
+      {#if currentPage - 2 <= page && page <= currentPage + 2}
+        <button class="cursor-pointer px-2 py-1 rounded hover:bg-gray-200" aria-current={currentPage === page} on:click={() => goToPage(page)}>
+          {page}
+        </button>
+      {/if}
+    {/each} -->
+
+    <!-- {#if currentPage < totalPages - 2}
+      <span class="cursor-pointer px-2 py-1 rounded hover:bg-gray-200">...</span>
+    {/if} -->
+
+    <button class="cursor-pointer px-2 py-1 rounded hover:bg-gray-200" disabled={currentPage === totalPages} on:click={() => goToPage(currentPage + 1)}>
+      Next
+    </button>
+    <button class="cursor-pointer px-2 py-1 rounded hover:bg-gray-200" disabled={currentPage === totalPages} on:click={() => goToPage(totalPages)}>
+      Last
+    </button>
+  </div>
 </section>	
 
